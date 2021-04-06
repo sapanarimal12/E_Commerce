1fa452735f6b2eb7906fca36d52f398b2c98a616
sapanarimal12 <rimalsapanaaa55@gmail.com>
2021-04-06 12:55:06 +0545

    signup

--- home/models.py
+++ home/models.py
@@ -15,6 +15,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_cat_url(self):
+        return reverse("home:category", kwargs={'slug': self.slug})
+
 class Slider(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='media')
@@ -58,13 +61,17 @@ class Item(models.Model):
     slug = models.CharField(max_length=200, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE,null=True)
-
+    description = models.TextField(blank=True)
+    specification = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
 
     def get_item_url(self):
         return reverse("home:products", kwargs={'slug': self.slug})
+
+#class Contact(models.Model):
+
 
 
 
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from .models import *
 
@@ -41,7 +43,64 @@ class ItemDetailView(BaseView):
 
         cat = Item.objects.get(slug=slug).category_id
         self.views['catitems'] = Item.objects.filter(category=cat)
-        return render(request,'product-detail.html',self.views)
+        return render(request, 'product-detail.html', self.views)
+
+class CategoryView(BaseView):
+    def get(self, request, slug):
+        cat_id = Category.objects.get(slug=slug).id
+        self.views['catdetail'] = Item.objects.filter(category= cat_id)
+        return render(request, 'product-list.html', self.views)
+
+class SearchView(BaseView):
+    def get(self, request):
+        #query = request.GET.get('search', None)
+        if request.method == 'GET':
+            query = request.GET['search']
+            self.views['search_product'] = Item.objects.filter(title__icontains=query)
+            return render(request, 'search.html', self.views)
+
+        return render(request, 'search.html')
+
+def signup(request):
+    if request.method == 'POSt':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        if password == cpassword:
+            if User.obljects.filter(username = username).exists():
+                messages.error(request, 'This user name is already taken.')
+                return redirect('home:account')
+
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'This email is already taken.')
+                return redirect('home:account')
+
+            else:
+                user = User.objects.create_user(
+                    username = username,
+                    email = email,
+                    password = password,
+                    first_name = fname,
+                    last_name = lname
+                )
+                user.save()
+                messages.success(request, 'You are registered.')
+                return redirect('/')
+
+        else:
+            messages.error(request, 'These password do not match.')
+            return redirect('home:account')
+
+    return render(request, 'signup.html')
+
+
+
+
+
+
 
 
 
